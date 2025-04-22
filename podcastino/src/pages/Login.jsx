c9a9bb657cad4ac71993a50e26b60a5b6064a1fc
Parsaspeed <92843741+Parsavazifeh@users.noreply.github.com>
2025-04-22 19:34:00 +0330

    Profile page done

--- podcastino/src/pages/Login.jsx
+++ podcastino/src/pages/Login.jsx
@@ -50,7 +50,7 @@ export default function Login() {
     }
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/api/login/", {
+      const response = await fetch("https://podcastino.darkube.app/api/login/", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
